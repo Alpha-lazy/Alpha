2f6c6c3d9f9071d5c40d8303fe16c4e8803beeec
Alpha-lazy <anishmore712@gmail.com>
2024-05-09 18:51:56 +0530

    initial update

---
+++ connect.js
@@ -0,0 +1,28 @@
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://Alpha:Anish%402007@cluster0.mqndugn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+  
+    await client.connect();
+  
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+    await client.close();
+  }
+}
+
+module.exports =run;
+
--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const app = express();
 const port = 4000;
+const connect = require('./connect')
 
-
-app.get("/" , (req,res)=>{
-    res.send("We are in the server")
+app.get("/" , async(req,res)=>{
+    try {
+        await connect()  
+        res.send("We are in the server")
+    } catch (error) {
+        res.send("We are not in the server")
+        
+    }
+    
 })
 
 
-app.listen(port, ()=>{
-    console.log("app listen at port 4000");
+app.listen(port, async()=>{
+    try {
+        
+        console.log("app listen at port 4000");
+    } catch (error) {
+        console.log("app does not listen at port 4000");
+        
+    }
 })
