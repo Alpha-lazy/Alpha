8519383a3beb1e7673a3ea1ef598a9bfa56f2eec
Alpha-lazy <anishmore712@gmail.com>
2024-05-09 19:12:26 +0530

    initial update

--- connect.js
+++ connect.js
@@ -1,6 +1,6 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://Alpha:Anish%402007@cluster0.mqndugn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://Anish:Anish%402007@cluster0.6c1gzun.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -13,10 +13,13 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-  
+    // .command({ ping: 1 })
     await client.connect();
   
-    await client.db("admin").command({ ping: 1 });
+   let collection =  await client.db("songs2").collection('books');
+
+   let data = await collection.find({}).toArray()
+   console.log(data);
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     
